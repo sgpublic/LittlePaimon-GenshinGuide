2e4e0e5741a5baacfdb18641a4d081888ace751e
sgpublic <sgpublic2002@gmail.com>
2024-06-12 13:28:19 +0800

    chore: remove all file

--- src/main/kotlin/io/github/sgpublic/genshinguide/core/logback/ConsoleFilter.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sgpublic.genshinguide.core.logback
-
-import io.github.sgpublic.kotlin.core.logback.filter.ConsoleFilter
-import io.github.sgpublic.genshinguide.Config
-
-open class ConsoleFilter: ConsoleFilter(
-    debug = Config.debug,
-    baseName = "io.github.sgpublic.genshinguide"
-)
